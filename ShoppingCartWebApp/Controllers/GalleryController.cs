f2167ef4c1c80efa86669706b50a65bd789e67cd
Daniel <danielchua93@gmail.com>
2021-10-13 00:26:25 +0800

    initial commit for gallery view and controller

---
+++ ShoppingCartWebApp/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShoppingCartWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ShoppingCartWebApp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
